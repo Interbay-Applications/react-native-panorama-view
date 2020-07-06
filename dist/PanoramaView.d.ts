b4fdd6bc7beff2523190e6c13f4d2da70668d979
Sean Kovacs <sean@seankovacs.com>
2020-07-06 15:04:37 -0400

    added dist to git so it flows into git repo package

---
+++ dist/PanoramaView.d.ts
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { ViewStyle } from "react-native";
+interface Props {
+    imageUrl: string;
+    imageData: string;
+    dimensions?: {
+        width: number;
+        height: number;
+    };
+    inputType?: "mono" | "stereo";
+    enableTouchTracking?: boolean;
+    onImageLoadingFailed?: () => void;
+    onImageLoaded?: () => void;
+    style: ViewStyle;
+}
+export declare const PanoramaView: React.FC<Props>;
+export {};
